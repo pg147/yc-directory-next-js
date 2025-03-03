17e7ebf448dfb02a7d5bf7a3520655cbee43da63
pg147 <xyzprathamesh147@gmail.com>
2025-03-03 17:57:04 +0530

    created: Provider Component

---
+++ app/components/Provider.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { SessionProvider } from 'next-auth/react'
+
+export default function Provider({ children }: { children: React.ReactNode }) {
+    return (
+        <SessionProvider>
+            {children}
+        </SessionProvider>
+    )
+}
