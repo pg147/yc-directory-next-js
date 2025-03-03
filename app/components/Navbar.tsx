83835071a41b243a134eb69def9a76c84d211811
pg147 <xyzprathamesh147@gmail.com>
2025-03-03 17:07:59 +0530

    Base setup done

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,26 @@
+import Link from 'next/link'
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <header className='px-5 py-3 bg-white shadow-sm font-work-sans'>
+        <nav className='flex justify-between items-center'>
+            <Link href={'/'}>
+
+            </Link>
+            
+            <ul className='flex items-center space-x-6'>
+                <li>
+
+                </li>
+                <li>
+
+                </li>
+                <li>
+
+                </li>
+            </ul>
+        </nav>
+    </header>
+  )
+}
