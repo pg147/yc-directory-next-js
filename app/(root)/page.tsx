b514602346ac175daad1696176318af08528135f
pg147 <xyzprathamesh147@gmail.com>
2025-03-06 22:04:56 +0530

    updated: Home - searchParams with query

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,16 @@
+import { SearchForm } from "../components";
 
-export default function Home() {
+export default async function Home({ searchParams }: { searchParams: Promise<{ query?: string }> }) {
+  const query = (await searchParams).query;
+
   return (
     <>
-      <h1 className="text-2xl">Home</h1>
+      <section className="pink_container">
+        <h1 className="heading">Pitch your ideas, <br /> Connect with Entreprenuers!</h1>
+        <p className="sub-heading !max-w-3xl">Submit ideas both on pitches and get noticed in virtual competitions</p>
+
+        <SearchForm query={query} />
+      </section>
     </>
   );
 }
